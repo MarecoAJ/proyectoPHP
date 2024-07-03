158ad749d7e06c638e311075025268d3082f896b
Alejandro J Mareco <a.j.mareco.cit@gmail.com>
2024-07-03 20:18:09 -0300

    agregarComentario test

--- tests/PostTest.php
+++ tests/PostTest.php
@@ -1,4 +1,16 @@
 <?php
 use PHPUnit\Framework\TestCase;
+use App\Post;
+use App\Comentario;
 
+class PostTest extends TestCase{
+    public function test_agregar_comentario(){
+        $post = new Post();
+        $comentario = new Comentario();
+        $post-> agregarComentario($comentario);  
+        
+        $this-> assertEquals(1, $post->contarComentarios());
+        $this-> assertInstanceOf(Comentario::class, $post->obtenerComentarios()[0]);
+    }
+}
 ?>
